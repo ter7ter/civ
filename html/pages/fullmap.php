f60cfadc3dfefa5560c9f94635b1f9cccf66ebf2
ter7ter <ter766@gmail.com>
2026-01-29 00:26:50 +0300

    Оптимизация загрузки fullmap

--- html/pages/fullmap.php
+++ html/pages/fullmap.php
@@ -27,6 +27,24 @@ if (!isset($_SESSION["game_id"])) {
 
             // Загружаем все клетки для первой планеты
             $allCells = Cell::getAllByPlanet($firstPlanet->id);
+
+            // Предварительно загружаем все города для этой планеты
+            $cities = MyDB::query(
+                "SELECT id, x, y, title, population FROM city WHERE planet = :planet_id",
+                ["planet_id" => $firstPlanet->id],
+            );
+
+            // Создаем индексированный массив городов для быстрого доступа
+            $cities_by_coords = [];
+            foreach ($cities as $city_data) {
+                $key = $city_data["x"] . "_" . $city_data["y"];
+                $cities_by_coords[$key] = [
+                    "id" => $city_data["id"],
+                    "title" => $city_data["title"],
+                    "population" => $city_data["population"],
+                ];
+            }
+
             // Формируем массив данных для карты
             $map_data = [];
             foreach ($allCells as $cell_data) {
@@ -38,25 +56,18 @@ if (!isset($_SESSION["game_id"])) {
                     $owner_color = $user_colors[$cell_data["owner"]];
                 }
 
-                // Проверяем, есть ли город на этой клетке
-                $city = City::by_coords(
-                    $cell_data["x"],
-                    $cell_data["y"],
-                    $cell_data["planet"],
-                );
+                // Проверяем, есть ли город на этой клетке, используя предварительно загруженные данные
+                $key = $cell_data["x"] . "_" . $cell_data["y"];
+                $city_data = isset($cities_by_coords[$key])
+                    ? $cities_by_coords[$key]
+                    : null;
 
                 $map_data[$cell_data["x"]][$cell_data["y"]] = [
                     "x" => $cell_data["x"],
                     "y" => $cell_data["y"],
                     "type" => $cell_data["type_id"],
                     "owner" => $owner_color ? ["color" => $owner_color] : null,
-                    "city" => $city
-                        ? [
-                            "id" => $city->id,
-                            "title" => $city->title,
-                            "population" => $city->population,
-                        ]
-                        : null,
+                    "city" => $city_data,
                 ];
             }
         }
